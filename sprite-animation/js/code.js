8bef0c5186121c8e73ceb9b3daf85188943abb87
felipemaxplay <felipemaxplay@gmail.com>
2022-02-16 20:25:07 -0300

    simple animate method

--- sprite-animation/js/code.js
+++ sprite-animation/js/code.js
@@ -16,12 +16,22 @@ const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
+let gameFrame = 0;
+const staggerFrame = 6;
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
     ctx.drawImage(playerImg, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if(gameFrame % staggerFrame == 0) {
+        if(frameX < 6) {
+            frameX++;
+        } else {
+            frameX = 0;
+        }
+    }
+
+    gameFrame++;
     requestAnimationFrame(animate);
 }
-
 animate();
