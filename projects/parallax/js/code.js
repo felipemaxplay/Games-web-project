90605afddf78b2ce0aec209279ec6de8d5affd02
felipemaxplay <felipemaxplay@gmail.com>
2022-02-24 22:13:56 -0300

    class layers

--- projects/parallax/js/code.js
+++ projects/parallax/js/code.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 2;
+let gameSpeed = 5;
 
 const backGroundLayer1 = new Image();
 backGroundLayer1.src = "./assets/layer-1.png";
@@ -16,18 +16,50 @@ backGroundLayer4.src = "./assets/layer-4.png";
 const backGroundLayer5 = new Image();
 backGroundLayer5.src = "./assets/layer-5.png";
 
-let x = 0;
-let x2 = 2400;
+class Layer {
+    constructor(image, speedMofier) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.x2 = this.width;
+        this.image = image;
+        this.speedMofier = speedMofier;
+        this.speed = gameSpeed * this.speedMofier;
+    }
+    
+    update() {
+        this.speed = gameSpeed * this.speedMofier;
+        if(this.x <= -this.width) {
+            this.x = this.width + this.x2 - this.speed;
+        }
+        if(this.x2 <= -this.width) {
+            this.x2 = this.width + this.x - this.speed;
+        }
+        this.x = Math.floor(this.x - this.speed);
+        this.x2 = Math.floor(this.x2 - this.speed);
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+
+const layer1 = new Layer(backGroundLayer1, 0.1);
+const layer2 = new Layer(backGroundLayer2, 0.2);
+const layer3 = new Layer(backGroundLayer3, 0.3);
+const layer4 = new Layer(backGroundLayer4, 0.4);
+const layer5 = new Layer(backGroundLayer5, 0.5);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backGroundLayer4, x, 0);
-    ctx.drawImage(backGroundLayer4, x2, 0);
-
-    if(x < -2400) { x = 2400 - gameSpeed; }    
-    x -= gameSpeed;
-    if(x2 < -2400) { x2 = 2400 - gameSpeed; }    
-    x2 -= gameSpeed;
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    });
 
     requestAnimationFrame(animate);
 }
